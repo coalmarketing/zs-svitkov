2bf691b9db99edafca8234c79a532d0e55072722
Michal Špitálský <michal.spitalsky@pm.me>
2026-02-10 11:32:18 +0100

    style (*): html sanitisation

--- lib/api/html/sanitize.ts
+++ lib/api/html/sanitize.ts
@@ -2,12 +2,19 @@ import sanitizeHtml from "sanitize-html";
 
 export function sanitise(content: string) {
   return sanitizeHtml(content, {
-    allowedTags: ["p", "strong", "em", "ul", "ol", "li", "h3", "a"],
-    allowedAttributes: { a: ["href", "target", "rel"] },
+    allowedTags: ["p", "strong", "em", "ul", "ol", "li", "h3", "a", "span"],
+    allowedAttributes: {
+      a: ["href", "target", "rel"],
+      span: ["style"],
+    },
     transformTags: {
       a: sanitizeHtml.simpleTransform("a", {
         target: "_blank",
         rel: "noopener noreferrer",
+      }),
+
+      div: sanitizeHtml.simpleTransform("span", {
+        style: "display: inline-block; margin-right: 0.25em;",
       }),
     },
   });
